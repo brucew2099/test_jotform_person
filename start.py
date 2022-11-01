91dc177b7c37187a7a0d8100731cf96bea6cbafb
brucew2099 <alankwok1@gmail.com>
2022-11-01 11:15:21 -0400

    Simplify the process by taking out Sqlalchemy

--- start.py
+++ start.py
@@ -2,13 +2,23 @@
 Imports
 """
 
+import json
 import os
-from flask import render_template, request
-from app import create_app
-from models import Person
+from os.path import abspath, dirname, join
+from dotenv import load_dotenv
+import pyodbc
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+basedir = abspath(dirname(__file__))
+load_dotenv(join(basedir, '.env-local'))
+
+DRIVER = os.environ.get('SQLSERVER.DRIVER')
+USER = os.environ.get('SQLSERVER.USER')
+PASSWD = os.environ.get('SQLSERVER.PASSWD')
+HOST = os.environ.get('SQLSERVER.HOST')
+DB = os.environ.get('SQLSERVER.DB')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -23,9 +33,50 @@ def process_jotform():
     This is the process JotForm route
     """
     form_data = request.form.to_dict()
-
     if form_data["rawRequest"]:
-        Person().parse_patient_data(form_data)
+        req = json.loads(form_data["rawRequest"])
+
+        conn = pyodbc.connect(f'Driver={DRIVER}; Server={HOST}; \
+            Database={DB}; UID={USER}; PWD={PASSWD}; TrustedConnection=True')
+
+        cursor = conn.cursor()
+
+        next_id = 1
+
+        try:
+            cursor.execute("SELECT IDENT_CURRENT('Persons') + 1")
+            next_id = 1 if cursor.fetchone()[0] is None else cursor.fetchone()[0]
+        except Exception as exception:
+            print(exception)
+        finally:
+            print('There is no record in the table')
+            print(f'next_id: {next_id}')
+
+        cursor.execute("INSERT INTO Persons \
+            (Id, FirstName, LastName, Age, FormId, SubmissionId, FormTitle) \
+            VALUES (?, ?, ?, ?, ?, ?, ?)", (
+                next_id,
+                req['q3_name']['first'], \
+                req['q3_name']['last'], \
+                req['q4_age'], \
+                form_data['formID'], \
+                form_data['submissionID'],\
+                form_data['formTitle']))
+
+        # cursor.execute("INSERT INTO Persons \
+        #         (Id, FirstName, LastName, Age, FormId, SubmissionId, FormTitle) \
+        #         VALUES (?, ?, ?, ?, ?, ?, ?)", (
+        #         next_id, 'firstName', 'lastName', \
+        #         666, 'formId', 'submissionId', 'formTitle'))
+
+        conn.commit()
+
+        cursor.execute('SELECT * FROM Persons')
+        for i in cursor:
+            print(i)
+
+        cursor.close()
+        conn.close()
 
     return "ok", 200
 
