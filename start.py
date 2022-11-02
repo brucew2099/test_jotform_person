cf161d6faed2b5d9cd14572b591d4f0d087bee26
Alan Kwok <alankwok1@gmail.com>
2022-11-02 11:04:42 -0400

    Corrected whole bunch of typos in start.py

--- start.py
+++ start.py
@@ -11,14 +11,6 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
- 
-@app.route('/blogs')
-def blog():
-    app.logger.info('Info level log')
-    app.logger.warning('Warning level log')
-    return f"Welcome to the Blog"
- 
-app.run(host='localhost', debug=True)
 
 
 DRIVER = os.environ.get('SQLSERVER.DRIVER')
