2d4ea6d50597b4a3f1b6ee6843f7de161d0fff7c
brucew2099 <alankwok1@gmail.com>
2022-11-10 10:27:19 -0500

    Revamped the code

---
+++ app.py
@@ -0,0 +1,68 @@
+'''
+Imports
+'''
+from datetime import datetime
+import os
+import json
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+
+from models import Person, setup_db
+
+app = Flask(__name__, static_folder='static')
+csrf = CSRFProtect(app)
+
+# WEBSITE_HOSTNAME exists only in production environment
+if 'WEBSITE_HOSTNAME' not in os.environ:
+    # local development, where we'll use environment variables
+    print("Loading config.development and environment variables from .env file.")
+    app.config.from_object('azureproject.development')
+else:
+    # production
+    print("Loading config.production.")
+    app.config.from_object('azureproject.production')
+
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=app.config.get('DATABASE_URI'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
+
+db = setup_db(app)
+
+# Create databases, if databases exists doesn't issue create
+# For schema changes, run "flask db migrate"
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """
+        This is the main route
+    """
+    return render_template('index.html')
+
+@app.route('/process', methods=['GET', 'POST'])
+def process():
+    """
+    This is the process JotForm route
+    """
+    form_data = request.form.to_dict()
+    if form_data["rawRequest"]:
+        req = json.loads(form_data["rawRequest"])
+
+        person = Person()
+        person.first_name = req['q3_name']['first']
+        person.last_name = req['q3_name']['last']
+        person.age = req['q4_age']
+        person.form_id = form_data['formID']
+        person.submission_id = form_data['submissionID']
+        person.form_title = form_data['formTitle']
+        person.add_person_to_db()
+        
+    return render_template('addperson.html', person=person)
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,48 @@
+'''
+Imports the
+'''
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+# declarative base class
+Base = declarative_base()
+metadata = Base.metadata
+
+# Initialize the database connection
+db = SQLAlchemy()
+
+# Enable Flask-Migrate commands "flask db init/migrate/upgrade" to work
+migrate = Migrate()
+
+def setup_db(app):
+    '''
+    Setup the database
+    '''
+    db.app = app
+    db.init_app(app)
+    migrate.init_app(app, db)
+    return db
+
+session = db.session
+
+class Person(db.Model):
+    '''
+    This is the base class for all people
+    '''
+    __tablename__ = 'people'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    age = Column(Integer)
+    form_id = Column(String(50))
+    submission_id = Column(String(50))
+    form_title = Column(String(100))
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+    def person_to_db(self):
+        session.add(self)
+        session.commit()
